c414c7c2436cf6e5ebc5cf0490a899228cb59b5f
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2024-10-25 18:17:35 -0300

    Feat!  advances

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import { CarouselComponent } from '@/components/Cards'
 import { Faq } from '@/components/Faq'
 import { Footer } from '@/components/Footer'
 import { Hero } from '@/components/Hero'
-import { Register } from '@/components/Register'
 import { Tutorial } from '@/components/Tutorial'
 
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col mobile:px-5">
+      <div className="flex w-[100vw] flex-col">
         <Hero />
-        <Register />
         <CarouselComponent />
-        <Tutorial />
+        <div className="bg-zinc-950">
+          <Tutorial />
+        </div>
         <Faq />
       </div>
       <Footer />
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -13,37 +13,6 @@ export function CarouselComponent() {
   return (
     <div className="h-[100vh] justify-center mobile:h-full mobile:w-full mx-auto flex flex-col mt-40 mobile:mt-10 gap-10 laptop:max-w-[1200px] max-w-[1400px]">
       <div className="flex flex-col w-full gap-10">
-        <div className="text-4xl mobile:text-center">
-          <span className="text-[#dbc994]">Leilões em destaque</span>
-        </div>
-        <div className="w-[1400px] laptop:w-[1200px] mobile:w-[80%] mx-auto">
-          <Carousel
-            opts={{
-              align: 'start',
-            }}
-            className="w-full"
-          >
-            <CarouselContent>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                  <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-3xl font-semibold">
-                          {index + 1}
-                        </span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
-      </div>
-      <div className="flex flex-col w-full gap-10">
         <div className="text-4xl mobile:text-center text-[#dbc994]">
           Passo a Passo
         </div>
@@ -74,6 +43,38 @@ export function CarouselComponent() {
           </Carousel>
         </div>
       </div>
+      <div className="flex flex-col w-full gap-10">
+        <div className="text-4xl mobile:text-center">
+          <span className="text-[#dbc994]">Leilões em destaque</span>
+        </div>
+        <div className="w-[1400px] laptop:w-[1200px] mobile:w-[80%] mx-auto">
+          <Carousel
+            opts={{
+              align: 'start',
+            }}
+            className="w-full"
+          >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-3xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
+
       <div className="flex w-full justify-evenly">
         <div className="flex w-1/4 text-xl  justify-center py-2 rounded-xl text-black bg-[#dbc994] hover:bg-opacity-80 transition-all hover:scale-105 duration-300">
           Link 1
