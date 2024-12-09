c36ed06bdd81be7bdd50fd64fa0aadaafac8e0e6
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2024-12-09 00:28:14 -0300

    Fix!

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -252,9 +252,6 @@ export function AddPost() {
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date('1900-01-01')
-                        }
                         initialFocus
                       />
                     </PopoverContent>
