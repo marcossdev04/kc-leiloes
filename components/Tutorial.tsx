b80a22b9694db411053da6c224e32b36de5f7c56
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2025-01-16 19:13:53 -0300

    Fix!

--- components/Tutorial.tsx
+++ components/Tutorial.tsx
@@ -17,7 +17,7 @@ export function Tutorial() {
   return (
     <div className="w-[1600px] h-full mt-40 mb-20 mini:px-3 mobile:mb-10 mini:mb-10 mini:mt-10 mobile:mt-10 laptop:w-[1200px] mobile:w-full mini:w-full items-center mx-auto flex">
       <div className="absolute mobile:h-[400px] mini:hidden mobile:top-[80px] mobile:w-[350px] mobile:-right-[30px] inset-y-0 right-0 bg-[#dbc994] laptop:w-[500px] laptop:h-[650px] top-72  laptop:top-52 rounded-full blur-2xl h-[900px] w-[800px] translate-x-1/2"></div>
-      <div className="absolute inset-y-0 mobile:top-[370px] mobile:h-[190px] mini:hidden mobile:rounded-none mobile:-right-[100px] mobile:w-[270px] blur-md mobile:blur-sm  z-50 laptop:w-[800px] laptop:h-[350px] laptop:top-[800px]  bg-[#0F0F0F] top-[1050px] laptop:right-[30px]  rounded-full  h-[500px] right-[90px] w-[800px]"></div>
+      <div className="absolute inset-y-0 mobile:top-[350px] mobile:h-[190px] mini:hidden mobile:rounded-none mobile:-right-[100px] mobile:w-[270px] blur-md mobile:blur-sm  z-50 laptop:w-[800px] laptop:h-[350px] laptop:top-[800px]  bg-[#0F0F0F] top-[1050px] laptop:right-[30px]  rounded-full  h-[500px] right-[90px] w-[800px]"></div>
       <div className="w-full h-full flex justify-center gap-10 mobile:gap-2 mini:gap-2 flex-col">
         <div
           data-aos="zoom-out-right"
