ed2b19afd56cad6b4cca7d11f8e0ad1f319b91c4
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2025-02-21 11:10:36 -0300

    fix!

--- components/Tutorial.tsx
+++ components/Tutorial.tsx
@@ -14,6 +14,7 @@ import {
 import image1 from '@/assets/image1.jpeg'
 import image2 from '@/assets/image2.jpeg'
 import image3 from '@/assets/image3.jpeg'
+import Link from 'next/link'
 
 const CAROUSEL_IMAGES = [image1, image2, image3] as const
 
@@ -38,7 +39,10 @@ export function Tutorial() {
     <div className="w-[1600px] mobile:px-5 h-full mt-40 mobile:mt-0 mb-20 mini:px-3 mobile:mb-10 mini:mb-10 mini:mt-10 laptop:w-[1200px] mobile:w-full mini:w-full items-center mx-auto flex">
       <div className="absolute mobile:hidden  mobile:h-[400px] mini:hidden mobile:top-[80px] mobile:w-[350px] mobile:-right-[30px] inset-y-0 right-0 bg-[#dbc994] laptop:w-[500px] laptop:h-[650px] top-72  laptop:top-52 rounded-full blur-2xl h-[900px] w-[800px] translate-x-1/2"></div>
       <div className="absolute inset-y-0 mobile:hidden  mobile:top-[350px] mobile:h-[190px] mini:hidden mobile:rounded-none mobile:-right-[100px] mobile:w-[270px] blur-md mobile:blur-sm  z-50 laptop:w-[800px] laptop:h-[350px] laptop:top-[800px]  bg-[#0F0F0F] top-[1050px] laptop:right-[30px]  rounded-full  h-[500px] right-[90px] w-[800px]"></div>
-      <div className="w-full h-full flex justify-center gap-10 mobile:gap-2 mini:gap-2 flex-col">
+      <Link
+        href={'https://www.instagram.com/katiacasaes/'}
+        className="w-full h-full flex justify-center gap-10 mobile:gap-2 mini:gap-2 flex-col"
+      >
         <div className="bg-white/20 flex flex-col">
           <div className="flex justify-center text-xl font-medium py-1.5 border-b px-5">
             Post
@@ -88,7 +92,7 @@ export function Tutorial() {
             <div></div>
           </div>
         </div>
-      </div>
+      </Link>
       <div className="z-40 mobile:hidden w-full mini:hidden mobile:w-2/3 flex justify-end">
         <Image
           className="w-[600px] mobile:w-[200px] laptop:w-[450px]"
